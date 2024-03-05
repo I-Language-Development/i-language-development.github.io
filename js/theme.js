a797c8ee09fb37e4ec0be9e503df0de6d572b237
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2024-03-05 20:08:05 +0100

    Added svg support

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,17 @@
 let darkMode = localStorage.getItem("darkMode");
 
 const darkModeToggle = document.getElementById("theme-toggle");
+let svgElements = [].slice.call(document.getElementsByTagName("svg")).filter(element => ![].slice.call(element.classList).includes("social-icon"));
 
 const enableDarkMode = () => {
   document.body.classList.add("dark");
+  svgElements.forEach(element => element.querySelector("path").setAttribute("fill", "#fbfbfe"));
   localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark");
+  svgElements.forEach(element => element.querySelector("path").setAttribute("fill", "#010104"));
   localStorage.setItem("darkMode", null);
 };
 
